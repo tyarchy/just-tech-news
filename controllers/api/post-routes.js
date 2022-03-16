366a65b588f81316d909787f979e8b270cba9d88
tyarchy <ty.d.gregory@gmail.com>
2022-03-16 10:46:48 -0600

    the module code update

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -77,32 +77,23 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
-  Post.create({
-    title: req.body.title,
-    post_url: req.body.post_url,
-    user_id: req.session.user_id
-  })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  if (req.session) {
+    Post.create({
+      title: req.body.title,
+      post_url: req.body.post_url,
+      user_id: req.session.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 router.put('/upvote', (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
-    .then(updatedVoteData => res.json(updatedVoteData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.put('/upvote', (req, res) => {
-  // make sure the session exists first
   if (req.session) {
-    // pass session id along with all destructured properties on req.body
     Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
       .then(updatedVoteData => res.json(updatedVoteData))
       .catch(err => {
@@ -110,6 +101,30 @@ router.put('/upvote', (req, res) => {
         res.status(500).json(err);
       });
   }
+});
+
+router.put('/:id', (req, res) => {
+  Post.update(
+    {
+      title: req.body.title
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -4,7 +4,6 @@ async function upvoteClickHandler(event) {
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
-
   const response = await fetch('/api/posts/upvote', {
     method: 'PUT',
     body: JSON.stringify({
@@ -14,12 +13,12 @@ async function upvoteClickHandler(event) {
       'Content-Type': 'application/json'
     }
   });
-  
+
   if (response.ok) {
     document.location.reload();
   } else {
     alert(response.statusText);
   }
 }
-  
-  document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
+
+document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
