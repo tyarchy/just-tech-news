1753e471b7f89ccfe7f9b9608049efe5d646ec02
tyarchy <ty.d.gregory@gmail.com>
2022-03-16 10:26:21 -0600

    added some comments

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -99,28 +99,17 @@ router.put('/upvote', (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {
-  Post.update(
-    {
-      title: req.body.title
-    },
-    {
-      where: {
-        id: req.params.id
-      }
-    }
-  )
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-      res.json(dbPostData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+router.put('/upvote', (req, res) => {
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 router.delete('/:id', (req, res) => {
